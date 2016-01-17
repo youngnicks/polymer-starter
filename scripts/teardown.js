eecc9e997a4a9e90dfcd716569e466ba7d69f767
youngnicks <americanpilot84@gmail.com>
2016-01-17 14:08:20 -0500

    Initial commit.

---
+++ scripts/teardown.js
@@ -0,0 +1,8 @@
+'use strict';
+var db = require("org/arangodb").db;
+
+function dropCollection(name) {
+  var collectionName = applicationContext.collectionName(name);
+  db._drop(collectionName);
+}
+
